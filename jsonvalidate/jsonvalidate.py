36c631c385ea86369436cba963ced00afcb2268d
Robus Gauli <robusgauli@gmail.com>
2018-06-09 11:38:59 +0545

    Object type validation

--- jsonvalidate/jsonvalidate.py
+++ jsonvalidate/jsonvalidate.py
@@ -14,12 +14,15 @@ class Error(object):
         r.update({'type': self.__name__})
         return r
 
-class TypeError(Error):
+class _TypeError(Error):
     __name__ = 'TypeError'
 
     def __init__(self, expected, actual):
         self.expected = expected
         self.actual = actual
+
+class MissError(Error):
+    __name__ = 'MissingError'
 
 
 class Contract(object):
@@ -28,38 +31,88 @@ class Contract(object):
     def check(cls, val):
         pass
 
-class Type(Contract):
+class Type(object):
     """Abstract Base class for Type validation"""
+    __name__ = 'Type'
     _type = None
 
-    @classmethod
-    def check(cls, val):
-        if not isinstance(val, cls._type):
-            error = TypeError(cls._type.__name__, type(val).__name__)
-            return error.todict()
+    def __init__(self, optional=False):
+        self.optional = optional
+    
+    def check(self, val):
+        if not self.optional and val is None:
+            return True, MissError().todict()
 
+        if not self.optional and not isinstance(val, self._type):
+            return True, _TypeError(self.__name__, type(val).__name__).todict()
+        return False, None
+        
+        
 
 class String(Type):
     """Type Contract for String"""
+    __name__ = 'String'
     _type = str
 
 class Integer(Type):
     """Type Contract for Integer"""
+    __name__ = 'Integer'
     _type = int
 
 class Float(Type):
     """Type Contract for Float"""
+    __name__ = 'Float'
     _type = float
 
 class Boolean(Type):
+    __name__ = 'Boolean'
     """Type Contract for Boolean"""
     _type = bool
 
 
+class Object(Contract):
+    __name__ = 'Object'
+
+    def __init__(self, object_shape):
+        if not isinstance(object_shape, dict):
+            raise TypeError('Requires argument of type dict as a validation Schema.')
+        self.object_shape = object_shape
+    
+    def check(self, value):
+        # make sure that val of type is of dict
+        if not isinstance(value, dict):
+            return True, _TypeError(self.__name__, type(value).__name__).todict()
+        error = False
+        result = {}
+        for key, contract in self.object_shape.items():
+            # grab the value or None
+            _val = value.get(key)
+            print(contract.__class__.__name__)
+            _error, _result = contract.check(_val)
+            if _error:
+                error = True
+            result[key] = _result
+        return error, result
+    
+            
 
 def main():
-    s = String.check(3)
-    print(s)
+    schema = Object({
+        'name': String(),
+        'age': Integer(),
+        'address': Object({
+            'permanent': String(),
+            'temporary': String(optional=False)
+        })
+    })
+
+    payload = {
+        'name': 'robus',
+        'age': 34,
+        'address': 3
+
+    }
+    print(schema.check(payload))
 
 if __name__ == '__main__':
     main()

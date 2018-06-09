1e125c9bda2e44a30418691c1bd87f4ea0ef42a7
Robus Gauli <robusgauli@gmail.com>
2018-06-09 07:56:08 +0545

    Type definition for primitives

--- jsonvalidate/jsonvalidate.py
+++ jsonvalidate/jsonvalidate.py
@@ -1,3 +1,58 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""
+    jsonvalidate.jsonvalidate
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+    Module that provides a helper classes for defining schema and validation for json
+"""
+
+class Error(object):
+    
+    def todict(self):
+        return {'type': self.__class__.__name__, **vars(self)}
+
+class TypeError(Error):
+    
+    def __init__(self, expected, actual):
+        self.expected = expected
+        self.actual = actual
+
+
+class Contract(object):
+    """Descriptor protocol"""    
+    @classmethod
+    def check(cls, val):
+        pass
+
+class Type(Contract):
+    """Abstract Base class for Type validation"""
+    _type = None
+
+    @classmethod
+    def check(cls, val):
+        if not isinstance(val, cls._type):
+            error = TypeError(cls._type.__name__, type(val).__name__)
+            return error.todict()
+
+
+class String(Type):
+    _type = str
+
+class Integer(Type):
+    _type = int
+
+class Float(Type):
+    _type = float
+
+class Boolean(Type):
+    _type = bool
+
+
+
+def main():
+    s = String.check(3)
+    print(s)
+
+if __name__ == '__main__':
+    main()
+
